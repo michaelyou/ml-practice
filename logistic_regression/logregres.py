b49501623271365800281653aa9140905e576724
youwangqiu <youwangqiu@promote.cache-dns.local>
2017-03-17 00:36:33 +0800

    logistic regression

--- logistic_regression/logregres.py
+++ logistic_regression/logregres.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
 
 
 def load_dataset():
@@ -31,5 +33,58 @@ def grad_ascent(data_mat, class_labels):
         weights = weights + alpha * data_matrix.transpose() * error
     return weights
 
-data_mat, label_mat = load_dataset()
-print(grad_ascent(data_mat, label_mat))
+
+def stoc_grad_ascent(data_mat, class_labels):
+    m, n = data_mat.shape
+    alpha = 0.01
+    weights = np.ones(n)
+    for i in range(m):
+        h = sigmoid((data_mat[i] * weights).sum())
+        error = class_labels[i] - h
+        weights = weights + alpha * error * data_mat[i]
+    return weights
+
+
+def stoc_grad_ascent2(data_mat, class_labels):
+    m, n = data_mat.shape
+    data_mat = data_mat.as_matrix()
+    class_labels = class_labels.as_matrix()
+    weights = np.ones(n)
+    max_cycles = 150
+    for k in range(max_cycles):
+        data_index = list(range(m))
+        for i in range(m):
+            # 减小步长
+            alpha = 4 / (1.0 + k + i) + 0.0001
+            rand_index = int(np.random.uniform(0, len(data_index)))
+            h = sigmoid(np.sum(data_mat[rand_index] * weights))
+            error = class_labels[rand_index] - h
+            weights = weights + alpha * error * data_mat[rand_index]
+            del(data_index[rand_index])
+    return weights
+
+# data_mat, label_mat = load_dataset()
+# print(stoc_grad_ascent2(DataFrame(data_mat), DataFrame(label_mat)))
+
+
+def colic_test():
+    train_mat = pd.read_csv('horseColicTraining.txt', sep='\t', header=None)
+    m, n = train_mat.shape
+    train_label = train_mat.pop(n-1)
+
+    train_weights = stoc_grad_ascent2(train_mat, train_label)
+
+    test_mat = pd.read_csv('horseColicTest.txt', sep='\t', header=None)
+    test_label = test_mat.pop(test_mat.shape[1]-1)
+
+    err_count = 0
+    result = (test_mat.as_matrix() * train_weights).sum(axis=1)
+    for index, r in enumerate(result):
+        res = 1 if sigmoid(r) > 0.5 else 0
+        if res != test_label[index]:
+            err_count += 1
+        
+    print(err_count)
+    print('err rate is', err_count / len(test_label))
+
+colic_test()
